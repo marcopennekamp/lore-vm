03762d79b4777eb064cf24afea5bdf305588a10b
Marco Pennekamp <marco.pennekamp96@gmail.com>
2015-06-03 02:11:02 +0200

    Fix buffer being at length 0.

--- src/io.rs
+++ src/io.rs
@@ -6,14 +6,19 @@ use byteorder::{BigEndian, ReadBytesExt};
 pub fn read_string(read: &mut Read) -> String {
     let string_length = read.read_u16::<BigEndian>().unwrap() as usize;
     let mut string_bytes = Vec::with_capacity(string_length);
+    unsafe {
+        string_bytes.set_len(string_length);
+    }
 
     let result = read.read(&mut string_bytes[..]);
     if !result.is_ok() {
         panic!("Could not read string!");
     }
 
-    if result.unwrap() != string_length {
-        panic!("Read length does not equal the string length!")
+    let result = result.unwrap();
+    if result != string_length {
+        panic!("Read length {} does not equal the string length {}.",
+                result, string_length);
     }
 
     String::from_utf8(string_bytes).unwrap()
